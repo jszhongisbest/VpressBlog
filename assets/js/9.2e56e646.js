fe13522edcd3375e5032a917df156387dbfeaac3
GitHub Action <action@github.com>
2020-10-29 15:05:07 +0000

    deploy to Github pages

---
+++ assets/js/9.2e56e646.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{376:function(e,t,n){"use strict";n.r(t);n(51),n(79);var r={beforeCreate:function(){this.$router.replace("/")}},u=n(8),c=Object(u.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=c.exports}}]);
